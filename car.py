1613f52560e53ccc23d47279b48c0b4452e39f33
Max Ferguson <maxkferg@gmail.com>
2017-05-26 13:56:51 -0700

    Add command line interface for training

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from webserver.application.app import app, db
+from learning.train import train_car, train_simulation
 
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -8,10 +9,24 @@ manager = Manager(app)
 # migrations
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def create_db():
     """Creates the db tables."""
     db.create_all()
+
+
+@manager.command
+def simulate():
+    """Train the car using the simulation"""
+    train_simulation()
+
+
+@manager.command
+def train():
+    """Train the car using the physical hardware"""
+    train_car()
+
 
 if __name__ == '__main__':
     manager.run()
